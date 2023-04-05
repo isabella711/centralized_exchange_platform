0df845e71f8aff7d36c746660841489c4bdfe4e6
Beforethedark <96949256+Beforethedark@users.noreply.github.com>
2023-04-05 16:16:19 +0800

    update ethGen as function

--- server/walletGenerater/ethGen.js
+++ server/walletGenerater/ethGen.js
@@ -1,30 +1,12 @@
-//When in webpage, it can generate a button when clicked, showing the address and private key of a random account.
-
-import React, { useState } from 'react';
-import Web3 from 'web3';
-
-const MyComponent = () => {
-  const web3 = new Web3('https://sepolia.infura.io/v3/40745b7b9da3492dbeb0cd9dff114a2d');
-  const [account, setAccount] = useState(null);
-
-  const handleCreateAccount = () => {
-    const newAccount = web3.eth.accounts.create();
-    console.log(newAccount);
-    setAccount(newAccount);
-  };
-
+const Web3=require("web3");
+const createAccount = async() => {
+  const web3 = await new Web3('https://sepolia.infura.io/v3/40745b7b9da3492dbeb0cd9dff114a2d');
+  const newAccount = await web3.eth.accounts.create();
+  console.log(newAccount);
+  
   return (
-    <div>
-      <button onClick={handleCreateAccount}>Create Account</button>
-      {account && (
-        <div>
-          <p>Address: {account.address}</p>
-          <p>Private Key: {account.privateKey}</p>
-        </div>
-      )}
-
-    </div>
+    newAccount
   );
 };
 
-export default MyComponent;
+createAccount()
