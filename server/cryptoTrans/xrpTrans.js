f6c459e47a185174822393936e7b87f5164e92c4
Charles_CH.HO <a20041060@gmail.com>
2023-04-22 13:13:01 +0800

    remove unused code

--- server/cryptoTrans/xrpTrans.js
+++ server/cryptoTrans/xrpTrans.js
@@ -1,37 +1,29 @@
 //rcvAddress:rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe
-import React, { useEffect, useState } from "react";
-import "../css/xrp.css";
 import { RippleAPI } from "ripple-lib";
 import { Client, xrpToDrops, dropsToXrp } from "xrpl";
 const xrpl = require("xrpl");
-
-function Xrp() {
+function xrpTrans() {
   // const [serverInfo, setServerInfo] = useState({ "buildVersion:": "0.0.0" });
-  // useEffect(() => {
-  //   getServerInfo();
-  // }, []);
-  const net = "wss://s.altnet.rippletest.net:51233";
 
-  const [rcvAddress, setRcvAddress] = useState("");
-  const [senderPublicKey, setSenderPublicKey] = useState("");
-  const [paidAmount, setPaidAmount] = useState(0);
-  const [balance, setBalance] = useState(0);
-  const [wallet, setWallet] = useState("");
-  const [results, setResults] = useState("");
-  const [client] = useState(new Client("wss://s.altnet.rippletest.net:51233"));
-  const [paymentButtonText, setPaymentButtonText] = useState("Send XRP");
-  console.log(`paidAmount>>`, paidAmount);
+  const net = "wss://s.altnet.rippletest.net:51233";
+ 
+  const paidAmount= 0
+  const client = new Client(net)
 
   const standby_wallet = xrpl.Wallet.fromSeed(
     "sEd7VZB9Tie9VXowLyv5o7g3gjm3NEt"
   );
+
+  const rcvAddress = "";
+  const senderPublicKey= standby_wallet.publicKey
+  const senderWallet = standby_wallet.address
 
   async function sendPayment() {
     await client.connect();
     console.log("Creating a payment transaction");
     const prepared = await client.autofill({
       TransactionType: "Payment",
-      Account: "rsL5E12SuMh5DiJMFQBrpFcokjQ8bEbrYt",
+      Account: senderWallet,
       Amount: xrpToDrops(paidAmount),
       Destination: rcvAddress,
     });
@@ -48,45 +40,6 @@ function Xrp() {
     client.disconnect();
   }
 
-  function getRcvAddress(e) {
-    setRcvAddress(e.target.value);
-    console.log(`receiver address is: `, rcvAddress);
-  }
-
-  function getPaidAmount(e) {
-    setPaidAmount(e.target.value);
-    console.log(`paid amount is: `, paidAmount);
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-      {/* <button variant="text" onClick={createWallet("standby")}></button> */}
-
-      <button onClick={sendPayment}>{paymentButtonText}</button>
-
-      <label>
-        Amount:
-        <input
-          type="text"
-          name="paidAmount"
-          onChange={getPaidAmount}
-          value={paidAmount}
-        />
-      </label>
-      <label>
-        Destination Account:
-        <input
-          type="text"
-          name="rcvAddress"
-          onChange={getRcvAddress}
-          value={rcvAddress}
-        />
-      </label>
-
-      <p>{/* <i>{statusText}</i> */}</p>
-    </div>
-  );
 }
 
-export default Xrp;
+export default xrpTrans;
