fda6ce5e4b5fe087349c30d3fc18be2c9e061340
Beforethedark <96949256+Beforethedark@users.noreply.github.com>
2023-04-26 10:50:39 +0800

    Minor changes

--- server/walletGenerater/btcGen.js
+++ server/walletGenerater/btcGen.js
@@ -18,17 +18,27 @@ const privateKey = new bitcore.PrivateKey(bn,'testnet');
 console.log(privateKey,privateKey.toAddress().toString())
 
 
+const createbitcoinAccount = async () => {
+  let value = Buffer.from('cat horse shoe lightning awesome bitcoin');
+  let hash = bitcore.crypto.Hash.sha256(value);
+  let bn = bitcore.crypto.BN.fromBuffer(hash);
+  const privateKey = new bitcore.PrivateKey(bn,'testnet');
+  console.log(privateKey,privateKey.toAddress().toString())
+  return [privateKey,privateKey.toAddress().toString()];}
+
+
+
 
 
 //Method 2
 const axios = require('axios');
 
 //
-const createbitcoinAccount = async () => {
+const createbitcoinAccountAPI = async () => {
     try {
-      const call = await axios.post("https://api.blockcypher.com/v1/btc/main/wallets?token=d918fe8c97a348e39abdb9e5ff48543d");
+      const call = await axios.post("https://api.blockcypher.com/v1/btc/test3/wallets?token=d918fe8c97a348e39abdb9e5ff48543d");
       return call.address;
     } catch (error) {
       console.log("Error", error);
     }};
-module.exports = { createbitcoinAccount };
+module.exports = { createbitcoinAccount,createbitcoinAccountAPI };
