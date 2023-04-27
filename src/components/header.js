40ca418bb66bb6825dd656e01131da75ec2e4288
Charles_CH.HO <34810665+a20041060@users.noreply.github.com>
2023-04-27 17:43:06 +0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 
 // importing material UI components
 import AppBar from "@mui/material/AppBar";
@@ -8,32 +8,33 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useAuth } from "../AuthContext";
+const { xrpFetch } = require("../api");
 
-export default function Header() {
+export default function Header(props) {
+  const [xrpBalance, setXrpBalance] = useState();
+  const { isAuthenticated, wallets } = props;
+
+  useEffect(() => {
+    console.log(`>>>wallets`, wallets);
+    const xrpAddress = wallets.find(
+      (wallet) => wallet.currency_type === "XRP"
+    ).classicAddress;
+    console.log(`>>>xrpAddress`, xrpAddress);
+
+    xrpFetch(xrpAddress).then((res) => {
+      let balance = res.result.account_data.Balance;
+      console.log(`>>>xrpBalance`, balance);
+      setXrpBalance(balance);
+    });
+  }, []);
+  if (!isAuthenticated) {
+    return <></>;
+  }
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        {/*Inside the IconButton, we 
-           can render various icons*/}
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          {/*This is a simple Menu 
-             Icon wrapped in Icon */}
-          <MenuIcon />
-        </IconButton>
-        {/* The Typography component applies 
-           default font weights and sizes */}
-
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          GeeksforGeeks Header
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+      {JSON.stringify(wallets)}xrpBalance : {xrpBalance}
+    </Typography>
   );
 }
