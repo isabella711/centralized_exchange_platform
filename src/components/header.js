bfe9ca54d59ac7610f74db1210e82500b2cff7f3
Charles_CH.HO <34810665+a20041060@users.noreply.github.com>
2023-04-28 00:55:04 +0800

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
 
 // importing material UI components
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import { useAuth } from "../AuthContext";
-const { xrpFetch } = require("../api");
+const { xrpFetch, callExternalApi } = require("../api");
 
 export default function Header(props) {
   const [xrpBalance, setXrpBalance] = useState();
+  const [ethBalance, setEthBalance] = useState();
   const { isAuthenticated, wallets } = props;
   useEffect(() => {
     if (wallets.length > 0) {
@@ -20,19 +14,38 @@ export default function Header(props) {
         wallets?.find((wallet) => wallet.currency_type === "XRP")
           .classicAddress ?? null;
 
+      const ethAddress =
+        wallets?.find((wallet) => wallet.currency_type === "ETH")
+          .wallet_addres ?? null;
       xrpFetch(xrpAddress).then((res) => {
         let balance = res.result.account_data.Balance;
         setXrpBalance(balance);
       });
+      callExternalApi(ethAddress, "eth").then((res) => {
+        setEthBalance(res.data);
+      });
     }
   }, []);
+  useEffect(() => {
+    // }, []);
+    // const result = callExternalApi(
+    //   "rsL5E12SuMh5DiJMFQBrpFcokjQ8bEbrYt",
+    //   "xrp"
+    // ).then((res) => {
+    //   console.log(`<<<eth`, res);
+    // });
+    // console.log("result", result);
+  }, []);
+
   if (!isAuthenticated) {
     return <></>;
   }
 
   return (
     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-      {JSON.stringify(wallets)}xrpBalance : {xrpBalance}
+      {JSON.stringify(wallets)}
+      xrpBalance : {xrpBalance}
+      ethBalance:{ethBalance}
     </Typography>
   );
 }
