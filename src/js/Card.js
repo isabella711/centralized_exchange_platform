ffd4abee1cf17e3f992a819ed26e1faf44fca52e
Charles_CH.HO <34810665+a20041060@users.noreply.github.com>
2023-04-28 19:56:47 +0800

    Update Card.js

--- src/js/Card.js
+++ src/js/Card.js
@@ -5,23 +5,27 @@ import { Link, useNavigate } from "react-router-dom";
 import "../css/App.css";
 import { useSelector } from "react-redux";
 import LoadingSpinner from "./Spinner";
+import Header from "../components/Header";
 
 export default function Card() {
-  const { loading } = useSelector((state) => state.user);
+  const { loading, user, wallets } = useSelector((state) => state.user);
   return loading === "pending" ? (
     <LoadingSpinner />
   ) : (
-    <div className="productList">
-      {contents.map((contents) => (
-        <Products
-          key={contents.id}
-          image={contents.image}
-          name={contents.name}
-          price={contents.price}
-          ticket={contents.ticket}
-          details={contents.details}
-        />
-      ))}
-    </div>
+    <>
+      <Header isAuthenticated={user !== null} wallets={wallets} />
+      <div className="productList">
+        {contents.map((contents) => (
+          <Products
+            key={contents.id}
+            image={contents.image}
+            name={contents.name}
+            price={contents.price}
+            ticket={contents.ticket}
+            details={contents.details}
+          />
+        ))}
+      </div>
+    </>
   );
 }
