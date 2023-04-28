903dc9fbf4ead43fd770340362dc0c3c87ad7e6e
Charles_CH.HO <34810665+a20041060@users.noreply.github.com>
2023-04-28 19:52:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import Login from "./js/Login";
 import { MenuAppBar } from "./js/AppBar";
 import { useSelector } from "react-redux";
 import HomeLayout from "./components/HomeLayout";
-import Header from "./components/header";import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useAuth } from "./AuthContext";
-import Card from "./js/Card";
-import Register from "./js/Register";
-import Login from "./js/Login";
-import { MenuAppBar } from "./js/AppBar";
-import { useSelector } from "react-redux";
-import HomeLayout from "./components/HomeLayout";
 import { getUserWallets } from "./api";
 import {
   fetchUserWallets,
@@ -91,100 +82,6 @@ export default function App() {
           path="/"
           element={
             <HomeLayout userInfo={userInfo}>
-              <Card />
-            </HomeLayout>
-          }
-        />
-        <Route path="/deposit" element={<Deposit />} />
-      </Routes>
-    </Router>
-  );
-}
-
-import { getUserWallets } from "./api";
-import {
-  fetchUserWallets,
-  useWallet,
-  usersLoading,
-} from "./reducers/usersReducer";
-import { store } from "./store/store";
-import Deposit from "./components/DStripeContainer";
-import Payment from "./js/Payment";
-
-export default function App() {
-  const { login, user, authenticated } = useAuth();
-  const [currentForm, setCurrentForm] = useState("login");
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  };
-  const userInfo = useSelector((state) => state.user);
-  useEffect(() => {
-    if (userInfo.user !== null) {
-      const { user_id } = userInfo.user;
-      store.dispatch(usersLoading("idle"));
-      store.dispatch(fetchUserWallets(user_id));
-    }
-  }, [userInfo.user]);
-  useEffect(() => {
-    // callExternalApi("Ai5qKTxmXjJow3TkexjEWRDYq2Xd4s8X9GC9C3KKmZWS", "sol");
-    // userLogin("alice@gmail.com", "12345678").then((res) => {
-    //   console.log(`from app`, res);
-    // });
-    // userRegister("edan@gmail.com", "12345678").then((res) => {
-    //   console.log(`res.>>from app`, res);
-    // });
-    // callExternalApi(
-    //   "Ai5qKTxmXjJow3TkexjEWRDYq2Xd4s8X9GC9C3KKmZWS",
-    //   "sol"
-    // ).then(res=>{
-    //   console.log(`sol>>>`,res.data.result.value)
-    //   })
-    // xrpFetch("rsL5E12SuMh5DiJMFQBrpFcokjQ8bEbrYt").then(res=>{console.log(`xrpFetch>>>`,res)})
-  }, []);
-
-  return (
-    // <div>
-    //   <Router>
-    //     <Routes>
-    //       <Route path="/" element={<Card />} />
-    //       <Route path="/payment/:id" element={<Payment />} />
-    //       <Route path="/Xrp" element={<Xrp />} />
-    //       <Route path="/Register" element={<Register />} />
-    //       <Route path="/UserProfile" element={<UserProfile />} />
-    //     </Routes>
-    //   </Router>
-    // </div>
-
-    <Router>
-      <Routes>
-        <Route
-          path="/login"
-          element={
-            <HomeLayout>
-              <MenuAppBar
-                isAuthenticated={userInfo.user !== null}
-                isInLoginPage={true}
-              />
-              <div className="App">
-                {currentForm === "login" ? (
-                  <Login onFormSwitch={toggleForm} />
-                ) : (
-                  <Register onFormSwitch={toggleForm} />
-                )}
-              </div>
-            </HomeLayout>
-          }
-        />
-        <Route path="/payment" element={<Payment />} />
-        <Route
-          path="/"
-          element={
-            <HomeLayout>
-              <MenuAppBar isAuthenticated={userInfo.user !== null} />
-              <Header
-                isAuthenticated={userInfo.user !== null}
-                wallets={userInfo.wallets}
-              />
               <Card />
             </HomeLayout>
           }
