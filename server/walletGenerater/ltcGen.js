7e55591feedb60cd26cdca4992703045380044bc
joe3600 <joe3600@gmail.com>
2023-04-30 15:04:06 +0800

    Add files via upload

--- server/walletGenerater/ltcGen.js
+++ server/walletGenerater/ltcGen.js
@@ -1,3 +1,6 @@
+/*
+//This can create Litecoin Wallet by litecore-lib
+
 const litecore = require("litecore-lib");
 
 
@@ -23,6 +26,44 @@ const createLtcAccount = async () => {
   };
 	
 
+};
+*/
+
+/*
+
+// Create Wallet via Block.io
+// Unfortunately we have used all free quota
+// Let's assign one for prototype
+
+
+const createLtcAccount = async () => {
+	
+  try {
+	const newUUID = uuidv4();
+	const walletlabel = newUUID;
+	const api_key = "578f-0868-73d8-7aba";
+	const response = await axios.get(
+	  `https://block.io/api/v2/get_new_address/?api_key=${api_key}&label=${walletlabel}`
+	);
+	res.send(response.data);
+  } catch (error) {
+    res.status(500).send({ error: "Error fetching new address" });
+  }
+	
+
+};
+
+*/
+
+//Assign LTC Address for prototype
+//In real production, we can pay to block.io or run by litecoin-cli command 
+
+const createLtcAccount = async () => {
+	return {
+		privateKey: "",
+		publicKey: "",
+		address: "QS4juvqHcAno8woPZZGF2aDCbkRAS8MNiW",
+  };
 };
 
 module.exports = { createLtcAccount };
