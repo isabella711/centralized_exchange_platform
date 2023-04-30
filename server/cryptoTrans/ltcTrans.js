d09198d80a52a5666ae26233e33563cd5596d1b4
joe3600 <joe3600@gmail.com>
2023-04-30 18:31:32 +0800

    Add buy and sell LTC

--- server/cryptoTrans/ltcTrans.js
+++ server/cryptoTrans/ltcTrans.js
@@ -5,7 +5,15 @@ const BlockIo = require('block_io');
 const apiKey = "578f-0868-73d8-7aba";
 const secretPIN = "Roc19111010Prc19491001";
 const block_io = new BlockIo(apiKey);
+const centralAddress = "Qa3HpkiD8n9JGFGauFx6aZWNPepD2gkTE8";
 
+async function buyLitecoin(toAddress, sendamount) {
+	sendLitecoinTransaction(centralAddress,toAddress, sendamount);
+}
+
+async function sellLitecoin(sellAddress, sendamount) {
+	sendLitecoinTransaction(sellAddress, centralAddress, sendamount);
+}
 
 async function sendLitecoinTransaction(fromAddress, toAddress, sendamount) {
   try {
