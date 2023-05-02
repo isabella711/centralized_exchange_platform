0d0c3301d88d504093daa6a63f88d0f2a86740a0
Charles_CH.HO <a20041060@gmail.com>
2023-05-02 22:36:16 +0800

    Update xrpTrans.js

--- server/cryptoTrans/xrpTrans.js
+++ server/cryptoTrans/xrpTrans.js
@@ -2,19 +2,20 @@
 const RippleAPI = require("ripple-lib");
 const { Client, xrpToDrops, dropsToXrp } = require("xrpl");
 const xrpl = require("xrpl");
-function xrpTrans() {
+const centralPrivateKey = "sEd7VZB9Tie9VXowLyv5o7g3gjm3NEt"; // here is actually seed
+const centralAddress = xrpl.Wallet.fromSeed(centralPrivateKey).address;
+async function xrpTrans(rcvAddress, senderPrivateKey, amount) {
   // const [serverInfo, setServerInfo] = useState({ "buildVersion:": "0.0.0" });
 
   const net = "wss://s.altnet.rippletest.net:51233";
 
-  const paidAmount = 100;
+  // const paidAmount = 100;
   const client = new Client(net);
 
-  const standby_wallet = xrpl.Wallet.fromSeed(
-    "sEd7VZB9Tie9VXowLyv5o7g3gjm3NEt"
-  );
+  const standby_wallet = xrpl.Wallet.fromSeed(senderPrivateKey);
 
-  const rcvAddress = "rNKdWCufKPCXc8v9AwtjYkEFtZyRNTkkkp";
+  const receiveAddress = rcvAddress;
+  // "rNKdWCufKPCXc8v9AwtjYkEFtZyRNTkkkp";
   const senderPublicKey = standby_wallet.publicKey;
   const senderWallet = standby_wallet.address;
   console.log(`senderWallet>>>`, senderWallet);
@@ -24,7 +25,7 @@ function xrpTrans() {
     const prepared = await client.autofill({
       TransactionType: "Payment",
       Account: senderWallet,
-      Amount: xrpToDrops(paidAmount),
+      Amount: xrpToDrops(amount),
       Destination: rcvAddress,
     });
     console.log(`prepared rcvAddress is: `, rcvAddress);
@@ -34,15 +35,30 @@ function xrpTrans() {
 
     // -------------------------------------------------------- Submit signed blob
     const tx = await client.submitAndWait(signed.tx_blob);
-    console.log(`tx`, tx);
+    // console.log(`tx`, tx);
     console.log("Identifying hash:", signed.hash);
     console.log("Signed blob:", signed.tx_blob);
     client.disconnect();
+    return tx;
   }
-  sendPayment();
-  return { msg: "success" };
+  const tx = await sendPayment();
+  // console.log(`txxx`, tx);
+  return { tx: tx, msg: "OK" };
 }
 
-// xrpTrans();
+const buyXrp = async (rcvAddress, amount) => {
+  xrpTrans(rcvAddress, centralPrivateKey, amount);
+};
 
-module.exports = { xrpTrans };
+const sellXrp = async (senderPrivateKey, amount) => {
+  xrpTrans(centralAddress, senderPrivateKey, amount);
+};
+
+// xrpTrans();
+// console.log(
+//   `centralAddress>>>`,
+//   centralAddress,
+//   `centralPubkey>>>`,
+//   xrpl.Wallet.fromSeed(centralPrivateKey).publicKey
+// );
+module.exports = { xrpTrans, buyXrp, sellXrp };
